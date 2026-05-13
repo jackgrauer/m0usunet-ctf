8ec4083c2471f77826c74cd88d776654324f139c
jackgrauer <jackgrauer@gmail.com>
2026-05-13 12:38:02 -0400

    Drop --image-size to 80M now that the install footprint is half its old size

--- build/build-alpine.sh
+++ build/build-alpine.sh
@@ -17,7 +17,7 @@ fi
 
 "$TOOL/alpine-make-vm-image" \
   --image-format raw \
-  --image-size  128M \
+  --image-size  80M \
   --branch      v3.18 \
   --packages    "$(xargs < "$ROOT/build/packages.txt")" \
   --script-chroot \
